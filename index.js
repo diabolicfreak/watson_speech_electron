9e5f7c51deb3b9acd747c9adb4c9baf9c10e566f
diabolicfreak <diabolicfreak@gmail.com>
2018-04-05 10:59:52 +0530

    Basic text to speech setup

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
+const axios = require('axios');
+const watson = require('watson-developer-cloud');
+require('dotenv').config();
 
 const { app, BrowserWindow, ipcMain } = electron;
 let mainWindow;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`); 
 });
 
+/**Generate new token */
+var authorization = new watson.AuthorizationV1({
+  username: process.env.SPEECH_TO_TEXT_USERNAME,
+  password: process.env.SPEECH_TO_TEXT_PASSWORD,
+  url: 'https://stream.watsonplatform.net/authorization/api', // Speech tokens
+});
+
+authorization.getToken({
+  url: 'https://stream.watsonplatform.net/speech-to-text/api'
+},
+function (err, token) {
+  if (!token) {
+    console.log('error:', err);
+  } else {
+    console.log("token "+token);
+    mainWindow.webContents.send(
+      'watson:token',
+      token
+    );
+  }
+});
+ 
+/**Send length of media file */
 ipcMain.on('video:submit', (event, path)=>{
   ffmpeg.ffprobe(path, (err, metadata)=>{
     mainWindow.webContents.send(
@@ -16,3 +42,4 @@ ipcMain.on('video:submit', (event, path)=>{
     );
   });
 });
+
